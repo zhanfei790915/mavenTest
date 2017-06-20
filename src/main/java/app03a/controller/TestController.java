95bddea7b1d67d538f9330bcf813981b4b44eef2
zhanfei <zfd1@163.com>
2017-06-20 08:35:04 +0800

    commit

--- src/main/java/app03a/controller/TestController.java
+++ src/main/java/app03a/controller/TestController.java
@@ -1,22 +1,36 @@
 package app03a.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import app03a.service.TestService;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
+	@Autowired
+	private TestService test;
 	@RequestMapping(value = "/delete/{name}", method = { RequestMethod.GET,
 			RequestMethod.POST })
 	public String delete(@PathVariable String name) {
 		System.out.println("delete:"+name);
+		test.add();
 		return "test";
 	}
 	@RequestMapping("/save/{id}")
 	public String save(@PathVariable int id){
 		System.out.println("save:"+id);
 		return "save";
+	}
+	@RequestMapping("/doProcess")
+	public String process(HttpServletRequest request){
+		String name = request.getParameter("name");
+		System.out.println(name);
+		return "/app01/process";
 	}
 }
---
+++ src/main/java/app03a/service/TestService.java
@@ -0,0 +1,12 @@
+package app03a.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+	
+	public void add(){
+		System.out.println("add a object");
+	}
+
+}
